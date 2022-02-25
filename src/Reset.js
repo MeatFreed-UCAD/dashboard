da2997cdd6389f10659ea4296c4fc515561bcd51
tcy1999 <tcy9902@outlook.com>
2022-02-25 11:27:17 -0500

    Add login/register pages

---
+++ src/Reset.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { auth, getUserModel } from "./DataModel";
+import './styles/style.css';
+
+function Reset() {
+const userModel = getUserModel();
+  const [email, setEmail] = useState("");
+  const [user, loading, error] = useAuthState(auth);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (loading) return;
+    if (user) navigate("/dashboard");
+  }, [user, loading]);
+  return (
+    <div className="main">
+      <div className="inputContainer">
+        <input
+          type="text"
+          className="inputBox"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="E-mail Address"
+        />
+        <button
+          type="button" className="btn btn-primary"
+          onClick={() => userModel.sendPasswordReset(email)}
+        >
+          Send password reset email
+        </button>
+        <div>
+          New user? <Link to="/register">Register</Link> now.
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Reset;
